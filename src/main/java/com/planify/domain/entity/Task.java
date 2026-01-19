0188426ad573df4f78b5ced03af72bb2c9ffa3d0
rizwan-zafar <rizwan.zafar@systemsltd.com>
2026-01-19 12:44:15 +0500

    Task entity added

---
+++ src/main/java/com/planify/domain/entity/Task.java
@@ -0,0 +1,46 @@
+package com.planify.domain.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name="tasks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name="id", updatable = false,nullable = false)
+    private UUID id;
+
+    @Column(name="title",nullable = false)
+    private String title;
+
+    @Column(name="description", length = 1000)
+    private String description;
+
+    @Column(name="due_date")
+    private LocalDate dueDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="status",nullable = false)
+    private TaskStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="priority",nullable = false)
+    private TaskPriority priority;
+
+    @Column(name="created", updatable = false,nullable = false)
+    private Instant created;
+
+    @Column(name="updated",nullable = false)
+    private Instant updated;
+
+}
