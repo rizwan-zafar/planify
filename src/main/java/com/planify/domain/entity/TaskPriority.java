228bce3ff7fa2ecc636b9514665c68310ed26beb
rizwan-zafar <rizwan.zafar@systemsltd.com>
2026-01-19 12:31:03 +0500

    required enums created for a Task

---
+++ src/main/java/com/planify/domain/entity/TaskPriority.java
@@ -0,0 +1,7 @@
+package com.planify.domain.entity;
+
+public enum TaskPriority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
---
+++ src/main/java/com/planify/domain/entity/TaskStatus.java
@@ -0,0 +1,6 @@
+package com.planify.domain.entity;
+
+public enum TaskStatus {
+    OPEN,
+    COMPLETE
+}
